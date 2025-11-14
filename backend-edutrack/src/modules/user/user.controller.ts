99c7612311959613f75bd23afd2b0eff6cdb18a3
JuanD911 <juandaviduarte911@gmail.com>
2025-11-13 19:48:50 -0500

    feat(user): Implement User module (service and controller)

--- backend-edutrack/src/modules/user/user.controller.ts
+++ backend-edutrack/src/modules/user/user.controller.ts
@@ -1,7 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, ValidationPipe } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUser } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post('createUser')
+  @UsePipes(ValidationPipe)
+  create(@Body() createuserDto: CreateUserDto) {
+    return this.userService.createuser(createuserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOneById(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateuserDto: UpdateUser) {
+    return this.userService.update(id, updateuserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(id);
+  }
 }
--- backend-edutrack/src/modules/user/user.service.ts
+++ backend-edutrack/src/modules/user/user.service.ts
@@ -1,4 +1,97 @@
-import { Injectable } from '@nestjs/common';
+import {BadRequestException, Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUser } from './dto/update-user.dto';
+import { Repository } from 'typeorm';
+import { UserEntity } from './entities/users.entities';
+import { InjectRepository } from '@nestjs/typeorm';
+import { isUUID } from 'class-validator';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private readonly logger = new Logger('UserService');
+
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async createuser(createuserDto: CreateUserDto) {
+    try {
+      const user = this.userRepository.create(createuserDto);
+      await this.userRepository.save(user);
+
+      return {
+        message: 'User was successfully saved',
+        user,
+      };
+    } catch (error) {
+      this.handlerErrors(error);
+    }
+  }
+
+  async findAll() {
+    try {
+      return await this.userRepository.find();
+    } catch (error) {
+      this.handlerErrors(error);
+    }
+  }
+
+  async findOneById(id: string) {
+    if (!isUUID(id))
+      throw new BadRequestException('The search term entered is not a valid ID');
+
+    try {
+      const user = await this.userRepository.findOne({
+        where: { id },
+      });
+
+      if (!user)
+        throw new NotFoundException(`User with id ${id} not found`);
+
+      return user;
+    } catch (error) {
+      this.handlerErrors(error);
+    }
+  }
+
+  async update(id: string, updateuserDto: UpdateUser) {
+    const user = await this.userRepository.preload({
+      id,
+      ...updateuserDto,
+    });
+
+    if (!user)
+      throw new NotFoundException(`User with id ${id} not found`);
+
+    try {
+      await this.userRepository.save(user);
+
+      return {
+        message: `The user with id ${id} was successfully updated`,
+        user,
+      };
+    } catch (error) {
+      this.handlerErrors(error);
+    }
+  }
+
+  async remove(id: string) {
+    const user = await this.userRepository.findOne({ where: { id } });
+
+    if (!user)
+      throw new NotFoundException(`User with id ${id} not found`);
+
+    try {
+      await this.userRepository.remove(user);
+      return `User with id ${id} has been deleted`;
+    } catch (error) {
+      this.handlerErrors(error);
+    }
+  }
+
+  handlerErrors(error: any) {
+    this.logger.error(error.message);
+    throw new BadRequestException(error.message);
+  }
+}
