983e6a2d987c2444da96e6230f4959ae9b795925
JuanD911 <juandaviduarte911@gmail.com>
2025-11-22 13:32:54 -0500

    feat(validation): Implement global ValidationPipe setup

--- backend-edutrack/src/main.ts
+++ backend-edutrack/src/main.ts
@@ -1,5 +1,6 @@
-import { NestFactory } from '@nestjs/core';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,7 +8,21 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
     methods: 'GET,POST,PATCH,DELETE,UPDATE',
-  })
+  });
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: false,
+    transform: true,
+    transformOptions: { enableImplicitConversion: true },
+    exceptionFactory: (errors) => {
+    console.error("🔥 VALIDATION ERRORS:", errors);
+      return new BadRequestException(errors);
+    }
+  }));
+
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
