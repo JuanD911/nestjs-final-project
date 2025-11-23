776a20c99713403c922271cc5a01e16a92f3baf3
JuanD911 <juandaviduarte911@gmail.com>
2025-11-23 18:57:09 -0500

    feat(config): Add CORS configuration

--- backend-edutrack/src/main.ts
+++ backend-edutrack/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'content-type',
+    credentials: true,
+
+  })
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
