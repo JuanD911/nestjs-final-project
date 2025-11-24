099ea3082429c577656b134bdcb35fcb80665166
JuanD911 <juandaviduarte911@gmail.com>
2025-11-24 17:08:32 -0500

    fix(studentForm): Change arranement of some elements of the student form

--- frontend-edutrack/src/components/student/studentForm.jsx
+++ frontend-edutrack/src/components/student/studentForm.jsx
@@ -4,15 +4,25 @@ import Button from '../ui/button/Button';
 import Form from '../ui/form/Form';
 import { createStudent } from "../../services/studentService";
 import { UserTypes } from '../../types/userTypes';
+import BackButton from "../layout/backButton/backButton";
+
+const initial_data = {
+    full_name: "",
+    email: "",
+    password: "",
+    entryYear: ""
+}
+
+const formPageLayoutStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center', 
+    gap: '15px', 
+    width: '100%',
+};
 
 function StudentForm () {
-
-    const [formData, setFormData] = useState({
-        full_name: "",
-        email: "",
-        password: "",
-        entryYear: "",
-    });
+    const [formData, setFormData] = useState(initial_data);
 
     const handleChange = (e) => {
         setFormData({
@@ -30,12 +40,13 @@ function StudentForm () {
                 full_name: formData.full_name,
                 email: formData.email,
                 password: formData.password,
-                role: UserTypes.student   // <--- importante
+                role: UserTypes.student
             }
         };
 
         try {
             const result = await createStudent(payload);
+            setFormData(initial_data);
             alert("Estudiante creado con éxito");
             console.log(result);
         } catch (error) {
@@ -44,57 +55,54 @@ function StudentForm () {
     };
 
     return(
-        <Form title='Registrar Estudiante' onSubmit={handleSubmit}>
-            <Input 
-                title='Nombre'
-                label='Nombre Completo'
-                type='text'
-                id='full_name'
-                name='full_name'
-                placeholder='Escribe el nombre completo'
-                value={formData.full_name}
-                onChange={handleChange}
-                required
-            />
-
-            <Input 
-                title='Correo Electrónico'
-                label='Correo Electrónico'
-                type='text'
-                id='email'
-                name='email'
-                placeholder='correo@ejemplos.com'
-                value={formData.email}
-                onChange={handleChange}
-                required
-            />
-
-            <Input 
-                title='Contraseña'
-                label='Contraseña'
-                type='password'
-                id='password'
-                name='password'
-                placeholder=''
-                value={formData.password}
-                onChange={handleChange}
-                required
-            />
-
-            <Input 
-                title='Año de Ingreso'
-                label='Año de Ingreso'
-                type='number'
-                id='entryYear'
-                name='entryYear'
-                placeholder='Año de Ingreso'
-                value={formData.entryYear}
-                onChange={handleChange}
-                required
-            />
-
-            <Button>Registrar Estudiante</Button>
-        </Form>
+        <>
+        <div style={formPageLayoutStyle}> 
+            <Form title='Registrar Estudiante' onSubmit={handleSubmit}>
+                <Input 
+                    label='Nombre Completo'
+                    type='text'
+                    id='full_name'
+                    name='full_name'
+                    placeholder='Escribe el nombre completo'
+                    value={formData.full_name}
+                    onChange={handleChange}
+                    required
+                />
+                <Input 
+                    label='Correo Electrónico'
+                    type='text'
+                    id='email'
+                    name='email'
+                    placeholder='correo@ejemplos.com'
+                    value={formData.email}
+                    onChange={handleChange}
+                    required
+                />
+                <Input 
+                    label='Contraseña'
+                    type='password'
+                    id='password'
+                    name='password'
+                    placeholder=''
+                    value={formData.password}
+                    onChange={handleChange}
+                    required
+                />
+                <Input 
+                    label='Año de Ingreso'
+                    type='number'
+                    id='entryYear'
+                    name='entryYear'
+                    placeholder='Año de Ingreso'
+                    value={formData.entryYear}
+                    onChange={handleChange}
+                    required
+                />
+                <Button>Registrar Estudiante</Button>
+            </Form>
+            <BackButton/> 
+        </div>
+        </>
     )
 }
 
