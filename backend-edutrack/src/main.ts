89a848f0382188ecf52bef6c2eb725a9e742727e
JuanD911 <juandaviduarte911@gmail.com>
2025-11-24 11:37:44 -0500

    fix(cors): Origin of the CORS fixed

--- backend-edutrack/src/main.ts
+++ backend-edutrack/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:5173',
     methods: 'GET,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'content-type',
     credentials: true,
