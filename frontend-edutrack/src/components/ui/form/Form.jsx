9d5be36e58dab0feeac2f46dbca2cf98b6f7f817
JuanD911 <juandaviduarte911@gmail.com>
2025-11-24 17:07:03 -0500

    feat(form): change arrangement of the elements of the form component

--- frontend-edutrack/src/components/ui/form/Form.jsx
+++ frontend-edutrack/src/components/ui/form/Form.jsx
@@ -1,9 +1,8 @@
 import './Form.css'
 
 export default function Form({ children, onSubmit, title}) {
-
     return (
-        <div>
+        <div className="form-container-card">
             <h2 className='form-title'>{title}</h2>
             <form onSubmit={onSubmit} className="space-y-4">
                 {children}
