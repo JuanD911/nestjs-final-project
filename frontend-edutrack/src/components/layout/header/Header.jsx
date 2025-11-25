7376f4f6bc59b38c58b1072e718327a8829c5a8a
JuanD911 <juandaviduarte911@gmail.com>
2025-11-24 19:54:05 -0500

    feat(header): Style header for theme change

--- frontend-edutrack/src/components/layout/header/Header.jsx
+++ frontend-edutrack/src/components/layout/header/Header.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
 import './Header.css';
+import ToggleButton from '../../ui/toggleButton/ToggleButton';
+import { useTheme } from '../../theme/ThemeProvider';
 
 const Header = () => {
+  const { alterTheme } = useTheme(); 
+
   return (
     <header className="app-header">
       <div className="logo-container">
-        {/* Usarías tu logo o un ícono aquí */}
         <span className="logo-icon">🎓</span> 
         <h1 className="app-title">EduTrack</h1>
       </div>
       
       <nav className="main-nav">
         <a href="../pages/mainPage" className="nav-link">Inicio</a>
-        <a href="/" className="nav-link">Estudiantes</a>
+        <a href="/students" className="nav-link">Estudiantes</a>
         <a href="/" className="nav-link">Profesores</a>
+        <a href="/" className="nav-link">Cursos</a>
+        <a href="/" className="nav-link">Inscripciones</a>
       </nav>
+      
+      <ToggleButton onToggle={alterTheme} /> 
+      
     </header>
   );
 };
