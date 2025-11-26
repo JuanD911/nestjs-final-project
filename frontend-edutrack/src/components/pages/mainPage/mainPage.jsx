f3734c3aae73513d00b59d4bfebe167842c7052f
JuanD911 <juandaviduarte911@gmail.com>
2025-11-26 11:51:15 -0500

    feat(mainPage): Add modules cards and navigation handlers to modules

--- frontend-edutrack/src/components/pages/mainPage/mainPage.jsx
+++ frontend-edutrack/src/components/pages/mainPage/mainPage.jsx
@@ -1,5 +1,5 @@
 import Card from "../../ui/card/Card";
-import { FaGraduationCap } from 'react-icons/fa';
+import { FaGraduationCap, FaChalkboardTeacher, FaFileSignature, FaBookOpen } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 
 function MainPage() {
@@ -8,15 +8,56 @@ function MainPage() {
   const goToStudentModule = () => {
     navigate('/students'); 
   };
+
+  const goToProfessorModule = () => {
+    navigate('/professors');
+  };
+
+  const goToEnrollmentModule = () => {
+    navigate('/enrollments');
+  };
+
+  const goToCourseModule = () => {
+    navigate('/courses');
+  };
+
+  const cardContainerStyle = {
+    display: 'flex',
+    gap: '30px',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    marginBottom: '30px',
+  };
   
   return (
-    <div className="main-page-grid">
+    <div style={cardContainerStyle}>
       <Card
         icon={FaGraduationCap}
         title="Módulo Estudiantes"
         description="Gestión completa de alumnos y registros."
         onClick={goToStudentModule}
       />
+      <Card
+        icon={FaChalkboardTeacher}
+        title="Módulo Profesores"
+        description="Gestión completa de profesores y registros."
+        onClick={goToProfessorModule}
+      />
+
+      <Card
+        icon={FaFileSignature}
+        title="Módulo Incripciones"
+        description="Gestión completa de incripciones."
+        onClick={goToEnrollmentModule}
+      />
+
+      <Card
+        icon={FaBookOpen}
+        title="Módulo Cursos"
+        description="Gestión de completa de cursos ."
+        onClick={goToCourseModule}
+      />
+
     </div>
   );
 }
