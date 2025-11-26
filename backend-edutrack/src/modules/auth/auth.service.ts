2abcddb46efac66722a1fd33dabb798ecaf58966
JuanD911 <juandaviduarte911@gmail.com>
2025-11-26 17:36:35 -0500

    feat(auth): Add auth module with JWT for a super user (admin) and hash password to all users

---
+++ backend-edutrack/src/modules/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from '../user/user.service';
+import * as bcrypt from 'bcryptjs';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(email: string, password: string) {
+    const user = await this.userService.findByEmailWithPassword(email);
+
+    if (!user) throw new UnauthorizedException('Invalid credentials');
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) throw new UnauthorizedException('Invalid credentials');
+
+    const payload = { id: user.id, role: user.role };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+      user,
+    };
+  }
+}
