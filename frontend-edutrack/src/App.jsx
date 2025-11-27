473d93f009df69e157b181e2d718cdd11a141ee8
JuanD911 <juandaviduarte911@gmail.com>
2025-11-27 11:45:39 -0500

    fix(app): Fix layout to not show the header on login page

--- frontend-edutrack/src/App.jsx
+++ frontend-edutrack/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/layout/header/Header'
 import Footer from './components/layout/footer/Footer'
 import MainPage from './components/pages/mainPage/mainPage';
@@ -19,49 +19,65 @@ import CoursePage from './components/pages/course/coursePage';
 import CourseForm from './components/course/courseForm';
 import CourseList from './components/course/courseList';
 import CourseEditForm from './components/course/courseEditForm';
+import Login from './components/pages/login/Login'
+
+
+function AppContent() {
+  const location = useLocation();
+
+  const hideHeaderRoutes = ['/login'];
+
+  const hideHeader = hideHeaderRoutes.includes(location.pathname);
+
+  return (
+    <div className="app-container">
+
+      {!hideHeader && <Header />}
+
+      <div className="main-content-wrapper">
+        <div className="main-content-inner">
+
+          <Routes>
+
+            <Route path="/login" element={<Login />} />
+
+            <Route path="/main" element={<MainPage />} />
+
+            <Route path="/students" element={<StudentPage />} />
+            <Route path="/students/new" element={<StudentForm />} />
+            <Route path="/students/list" element={<StudentList />} />
+            <Route path="/students/edit/:id" element={<StudentEditForm />} />
+
+            <Route path="/professors" element={<ProfessorPage />} />
+            <Route path="/professors/new" element={<ProfessorForm />} />
+            <Route path="/professors/list" element={<ProfessorList />} />
+            <Route path="/professors/edit/:id" element={<ProfessorEditForm />} />
+
+            <Route path="/enrollments" element={<EnrollmentPage />} />
+            <Route path="/enrollments/new" element={<EnrollmentForm />} />
+            <Route path="/enrollments/list" element={<EnrollmentList />} />
+
+            <Route path="/courses" element={<CoursePage />} />
+            <Route path="/courses/new" element={<CourseForm />} />
+            <Route path="/courses/list" element={<CourseList />} />
+            <Route path="/courses/edit/:id" element={<CourseEditForm />} />
+
+          </Routes>
+
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
+
 
 function App() {
   return (
     <Router>
       <ThemeProvider>
-        <div className="app-container">
-
-          <Header />
-
-          <div className="main-content-wrapper">
-            <div className="main-content-inner">
-
-              <Routes>
-
-                <Route path="/main" element={<MainPage />} />
-
-                <Route path="/students" element={<StudentPage />} />
-                <Route path="/students/new" element={<StudentForm />} />
-                <Route path="/students/list" element={<StudentList />} />
-                <Route path="/students/edit/:id" element={<StudentEditForm />} />
-
-                <Route path="/professors" element={<ProfessorPage />} />
-                <Route path="/professors/new" element={<ProfessorForm />} />
-                <Route path="/professors/list" element={<ProfessorList />} />
-                <Route path="/professors/edit/:id" element={<ProfessorEditForm />} />
-
-                <Route path="/enrollments" element={<EnrollmentPage />} />
-                <Route path="/enrollments/new" element={<EnrollmentForm />} />
-                <Route path="/enrollments/list" element={<EnrollmentList />} />
-
-                <Route path="/courses" element={<CoursePage />} />
-                <Route path="/courses/new" element={<CourseForm />} />
-                <Route path="/courses/list" element={<CourseList />} />
-                <Route path="/courses/edit/:id" element={<CourseEditForm />} />
-
-              </Routes>
-
-            </div>
-          </div>
-
-          <Footer />
-
-        </div>
+        <AppContent />
       </ThemeProvider>
     </Router>
   );
