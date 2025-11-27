7233cef7e97a84a3a541794c6a30f75655923d4b
JuanD911 <juandaviduarte911@gmail.com>
2025-11-27 12:00:47 -0500

    feat(auth): Add button to log out. Remove token from localStorage

--- frontend-edutrack/src/components/layout/header/Header.jsx
+++ frontend-edutrack/src/components/layout/header/Header.jsx
@@ -1,27 +1,45 @@
+import { useNavigate } from 'react-router-dom';
 import './Header.css';
 import ToggleButton from '../../ui/toggleButton/ToggleButton';
 import { useTheme } from '../../theme/ThemeProvider';
+import Button from '../../ui/button/Button';       
 
 const Header = () => {
   const { alterTheme } = useTheme(); 
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    console.log(`Log out`)
+    navigate('/login'); 
+
+  };
 
   return (
     <header className="app-header">
+      
       <div className="logo-container">
         <span className="logo-icon">🎓</span> 
         <h1 className="app-title">EduTrack</h1>
       </div>
       
       <nav className="main-nav">
-        <a href="../main" className="nav-link">Inicio</a>
+        <a href="/main" className="nav-link">Inicio</a>
         <a href="/students" className="nav-link">Estudiantes</a>
         <a href="/professors" className="nav-link">Profesores</a>
         <a href="/courses" className="nav-link">Cursos</a>
         <a href="/enrollments" className="nav-link">Inscripciones</a>
       </nav>
       
-      <ToggleButton onToggle={alterTheme} /> 
-      
+      <div className="action-container">
+        
+        <ToggleButton onToggle={alterTheme} /> 
+        
+        <Button onClick={handleLogout}> 
+          Cerrar Sesión
+        </Button>
+        
+      </div>
     </header>
   );
 };
