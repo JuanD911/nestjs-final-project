53df4c3d6206a42604633bc6fb65af9f6f9c0447
JuanD911 <juandaviduarte911@gmail.com>
2025-11-26 22:13:56 -0500

    refactor(auth): Include user role in JWT strategy payload result

--- backend-edutrack/src/modules/auth/jwt.strategy.ts
+++ backend-edutrack/src/modules/auth/jwt.strategy.ts
@@ -9,6 +9,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return payload;
+    return { id: payload.id, role: payload.role };
   }
 }
